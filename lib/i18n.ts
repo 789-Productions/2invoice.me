612e202a9de39042c18135918a19dbd63a8daa1f
jmart2004 <130313746+jmart2004@users.noreply.github.com>
2025-08-12 20:36:18 -0700

    Delete Invoices in Dashboard (#25)
    Can now delete invoices in dashboard through a button. Doesn't automatically update frontend.
    ---------
    
    Co-authored-by: Juan-Martinez-306 <130313746+Juan-Martinez-306@users.noreply.github.com>

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,13 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
 
 export default getRequestConfig(async ({requestLocale}) => {
-  // Typically corresponds to the `[locale]` segment
-  const requested = await requestLocale;
-  if (!requested) {
-    throw new Error("Locale not found");
-  }
   return {
-    locale: requested,
-    messages: (await import(`../messages/${requested}.json`)).default
+    locale: "en",
+    messages: (await import(`../messages/en.json`)).default
   };
 });
