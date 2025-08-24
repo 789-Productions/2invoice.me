21a8376507f4057503c5bd423632943731e78a39
jmart2004 <130313746+jmart2004@users.noreply.github.com>
2025-08-23 18:05:05 -0700

    Made some quick auth qol changes  (#40)
    Co-authored-by: Juan-Martinez-306 <130313746+Juan-Martinez-306@users.noreply.github.com>

--- app/[locale]/auth/signin/actions.ts
+++ app/[locale]/auth/signin/actions.ts
@@ -1,24 +1,25 @@
 'use server';
 
 import { signIn } from "@/lib/auth";
-import { AuthError } from 'next-auth';
-
 export async function authenticate(
-  prevState: string | undefined,
+  previousState: any,
   formData: FormData,
 ) {
-  try {
-    const { id, email } = await signIn('credentials', formData);
-    console.log('User authenticated:', { id, email });
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
+   try {
+    const credentials = Object.fromEntries(formData.entries());
+    await signIn('credentials', {...credentials, redirectTo: '/' + formData.get("locale") + '/dashboard'});
+  } catch (error: any) {
+    // do not remove this, it's neccessary for redirect to work
+    if (error.digest?.startsWith('NEXT_REDIRECT')) {
+      throw error;
     }
-    throw error;
+    if (error.cause?.err?.message) {
+      return { message: error.cause.err.message };
+    }
+    if (error.type == 'CredentialsSignin') {
+      return { message: 'Invalid credentials.' };
+    } else {
+      return { message: 'Something went wrong.' };
+    }
   }
 }
